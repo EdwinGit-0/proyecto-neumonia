34b89f35ed20dce61ac264d2c25b8db543e70921
EdwinGit0 <edwin.roquecd@gmail.com>
2026-09-08 00:59:24 -0400

    chore: create initial project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Clasificacion de imagenes de rayos X de torax para la identificacion de neumonia mediante redes neuronales convolucionales',
+    author='EDWIN ROQUE CERROGRANDE',
+    license='MIT',
+)
